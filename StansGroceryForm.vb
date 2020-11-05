42eabaede24c7c29df28a7e182c31857f2741d76
Lane Coleman <lane.coleman@live.com>
2020-11-05 01:00:18 -0700

    Finished separating the text file into 3 arrays. Items, Locations, and Categories.

--- StansGroceryForm.vb
+++ StansGroceryForm.vb
@@ -23,31 +23,79 @@ Public Class StansGroceryForm
 
 
 
-        Dim sizer, sizer2 As Integer
+        Dim foodSizer, foodSizer2, locSizer, locSizer2, catSizer, catSizer2 As Integer
 
+        'Items
         For i = 0 To UBound(arr)
             match = Regex.Match(arr(i), "ITM")
             If match.Success = True Then
-                sizer += 1
+                foodSizer += 1
             End If
         Next
 
-        Dim storeItems(sizer) As String
-        sizer2 = 0
+        Dim items(foodSizer) As String
+        foodSizer2 = 0
         For p = 0 To UBound(arr)
             match = Regex.Match(arr(p), "ITM")
             If match.Success = True Then
-                storeItems(sizer2) = arr(p)
-                sizer2 += 1
+                items(foodSizer2) = arr(p)
+                foodSizer2 += 1
             End If
         Next
 
-        For j = 0 To (UBound(storeItems) - 1)
-            storeItems(j) = Regex.Replace(storeItems(j), "Ω", "/")
-            storeItems(j) = Regex.Replace(storeItems(j), "ITM", String.Empty)
-            DisplayListBox.Items.Add(storeItems(j))
+        'LOCS
+        For l = 0 To UBound(arr)
+            match = Regex.Match(arr(l), "LOC")
+            If match.Success = True Then
+                locSizer += 1
+            End If
         Next
-        'i += 1
+
+        Dim locs(locSizer) As String
+        locSizer2 = 0
+        For k = 0 To UBound(arr)
+            match = Regex.Match(arr(k), "LOC")
+            If match.Success = True Then
+                locs(locSizer2) = arr(k)
+                locSizer2 += 1
+            End If
+        Next
+
+        'CATS
+        For y = 0 To UBound(arr)
+            match = Regex.Match(arr(y), "LOC")
+            If match.Success = True Then
+                catSizer += 1
+            End If
+        Next
+
+        Dim cats(catSizer) As String
+        catSizer2 = 0
+        For r = 0 To UBound(arr)
+            match = Regex.Match(arr(r), "CAT")
+            If match.Success = True Then
+                cats(catSizer2) = arr(r)
+                catSizer2 += 1
+            End If
+        Next
+
+
+
+        For j = 0 To (UBound(items) - 1)
+            items(j) = Regex.Replace(items(j), "Ω", "/")
+            items(j) = Regex.Replace(items(j), "ITM", String.Empty)
+            locs(j) = Regex.Replace(locs(j), "LOC", String.Empty)
+            cats(j) = Regex.Replace(locs(j), "CAT", String.Empty)
+            DisplayListBox.Items.Add(items(j))
+        Next
+
+
+
+
+        'This is for testing the other arrays, change between cats and locs
+        'For j = 0 To (UBound(cats) - 1)
+        '    DisplayListBox.Items.Add(cats(j))
+        'Next
     End Sub
     Private Function SplitWords(ByVal s As String) As String()
         Dim initialSplit As String = Regex.Replace(s, "/", "Ω")
